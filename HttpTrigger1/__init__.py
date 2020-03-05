f7f262251290d8425e9af8c52a7cc5a26353f33c
gargas <mlaisolutions@gmail.com>
2020-03-05 16:19:44 -0500

    returing json from dataframe

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -1,9 +1,9 @@
 import logging
-import azure.functions as func
 
+import azure.functions as func
 from azure.storage.blob import BlockBlobService
+import pandas as pd
 import tables
-import time
 
 STORAGEACCOUNTNAME= 'storagelabcoronavirus'
 STORAGEACCOUNTKEY= 'OcSGt1fCikgKjViIJTa6D0EcPLl2mK0YHVbxH25B7D4YV9VdwR/RYJ91sJqLbnh9janaMTOt6+fqChY+gsFFxQ=='
@@ -25,16 +25,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        #download from blob
-        t1=time.time()
         blob_service=BlockBlobService(account_name=STORAGEACCOUNTNAME,account_key=STORAGEACCOUNTKEY)
         blob_service.get_blob_to_path(CONTAINERNAME,BLOBNAME,LOCALFILENAME)
-        t2=time.time()
-        message = "File "+ BLOBNAME + "downloaded." 
+        #message = "File "+ BLOBNAME + "downloaded." 
+        df = pd.read_csv(LOCALFILENAME)
         dataframe_blobdata = pd.read_csv(LOCALFILENAME)
-        return func.HttpResponse(f"Hello {message}!")
+        #return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(dataframe_blobdata.to_json())
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
         )
