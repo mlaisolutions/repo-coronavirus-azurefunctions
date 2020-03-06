91de27b158c9ef026b0124d413b16a1b72b39450
gargas <mlaisolutions@gmail.com>
2020-03-05 21:06:24 -0500

    selecting only the latest date to return json

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -4,6 +4,7 @@ import azure.functions as func
 from azure.storage.blob import BlockBlobService
 import pandas as pd
 import tables
+#import geopandas as gpd
 
 STORAGEACCOUNTNAME= 'storagelabcoronavirus'
 STORAGEACCOUNTKEY= 'OcSGt1fCikgKjViIJTa6D0EcPLl2mK0YHVbxH25B7D4YV9VdwR/RYJ91sJqLbnh9janaMTOt6+fqChY+gsFFxQ=='
@@ -28,10 +29,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         blob_service=BlockBlobService(account_name=STORAGEACCOUNTNAME,account_key=STORAGEACCOUNTKEY)
         blob_service.get_blob_to_path(CONTAINERNAME,BLOBNAME,LOCALFILENAME)
         #message = "File "+ BLOBNAME + "downloaded." 
-        df = pd.read_csv(LOCALFILENAME)
-        dataframe_blobdata = pd.read_csv(LOCALFILENAME)
+        confirmed_df = pd.read_csv(LOCALFILENAME)
+        confirmed_df['Lat'] = confirmed_df['Lat'].astype(float)
+        confirmed_df['Long'] = confirmed_df['Long'].astype(float)
+        #confirmed = gpd.GeoDataFrame(confirmed_df, geometry=gpd.points_from_xy(confirmed_df.Long, confirmed_df.Lat))
+        confirmed_df = confirmed_df.loc[:, ['Province/State','Country/Region','Lat','Long',confirmed_df.columns[-1]]]
+        strGeoJson = confirmed_df.to_json()
         #return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-        return func.HttpResponse(dataframe_blobdata.to_json())
+        return func.HttpResponse(strGeoJson)
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
